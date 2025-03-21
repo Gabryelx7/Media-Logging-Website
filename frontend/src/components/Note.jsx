abfd16b20963de2f15fa7334794cc84dd853a71f
Gabryelx7 <gabrielmsd@live.com>
2025-03-21 08:26:57 -0300

    first commit

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/Note.css"
+
+function Note({ note, onDelete }) {
+    const formattedDate = new Date(note.created_at).toLocaleDateString("en-US")
+
+    return (
+        <div className="note-container">
+            <p className="note-title">{note.title}</p>
+            <p className="note-media-type">{note.media_type}</p>
+            <p className="note-score">{note.score}</p>
+            <p className="note-review">{note.review}</p>
+            <p className="note-date">{formattedDate}</p>
+            <button className="delete-button" onClick={() => onDelete(note.id)}>
+                Delete
+            </button>
+        </div>
+    );
+}
+
+export default Note
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+import Note from "../components/Note"
+import "../styles/Home.css"
+
+function Home() {
+    const [notes, setNotes] = useState([]);
+    const [review, setReview] = useState("");
+    const [title, setTitle] = useState("");
+    const [score, setScore] = useState("5");
+    const [media_type, setMediaType] = useState("Movie");
+
+    useEffect(() => {
+        getNotes();
+    }, []);
+
+    const getNotes = () => {
+        api
+            .get("/api/notes/")
+            .then((res) => res.data)
+            .then((data) => {
+                setNotes(data);
+                console.log(data);
+            })
+            .catch((err) => alert(err));
+    };
+
+    const deleteNote = (id) => {
+        api
+            .delete(`/api/notes/delete/${id}/`)
+            .then((res) => {
+                if (res.status === 204) alert("Note deleted!");
+                else alert("Failed to delete note.");
+                getNotes();
+            })
+            .catch((error) => alert(error));
+    };
+
+    const createNote = (e) => {
+        e.preventDefault();
+        api
+            .post("/api/notes/", { review, score, media_type, title })
+            .then((res) => {
+                if (res.status === 201) alert("Note created!");
+                else alert("Failed to make note.");
+                getNotes();
+            })
+            .catch((err) => alert(err));
+    };
+
+    return (
+        <div>
+            <div>
+                <h2>Notes</h2>
+                {notes.map((note) => (
+                    <Note note={note} onDelete={deleteNote} key={note.id} />
+                ))}
+            </div>
+            <h2>Create a Note</h2>
+            <form onSubmit={createNote}>
+                <label htmlFor="title">Title:</label>
+                <br />
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label htmlFor="media-type">Media Type:</label>
+                <br />
+                <select id="media-type" name="media-type" required onChange={(e) => setMediaType(e.target.value)}>
+                    <option value="Movie">Movie</option>
+                    <option value="Book">Book</option>
+                    <option value="Album">Album</option>
+                </select>
+                <br />
+                <label htmlFor="score">Score:</label>
+                <br />
+                <input
+                    type="number"
+                    id="score"
+                    name="score"
+                    min="0"
+                    max="10"
+                    step="1"
+                    required
+                    value={score}
+                    onChange={(e) => setScore(e.target.value)}
+                />
+                <label htmlFor="review">Review:</label>
+                <br />
+                <textarea
+                    id="review"
+                    name="review"
+                    required
+                    value={review}
+                    onChange={(e) => setReview(e.target.value)}
+                ></textarea>
+                <br />
+                <input type="submit" value="Submit"></input>
+            </form>
+        </div>
+    );
+}
+
+export default Home;
