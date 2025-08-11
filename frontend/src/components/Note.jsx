21dab5d62e47241001fb7fcd5ccf95f53b912bc0
Gabryelx7 <gabrielmsd@live.com>
2025-08-10 22:04:28 -0300

    Add update feature

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,30 +1,32 @@
-import React from "react";
 import "../styles/Note.css"
 
-function Note({ note, onDelete }) {
-    const formattedDate = new Date(note.created_at).toLocaleDateString("en-US")
-    const mediaTypeClass = `note-media-type-${note.media_type.toLowerCase()}`;
-    const getScoreColor = (score) => {
-        if (score <= 40) return "#f50000";
-        if (score <= 60) return "#f4cd0a";
-        if(score <= 80) return "#00ff00";
-        return "#009c00"; 
-    }
+function Note({ note, onDelete, onEdit }) {
+  const formattedDate = new Date(note.created_at).toLocaleDateString("en-US")
+  const mediaTypeClass = `note-media-type-${note.media_type.toLowerCase()}`;
+  const getScoreColor = (score) => {
+    if (score <= 40) return "#f50000";
+    if (score <= 60) return "#f4cd0a";
+    if (score <= 80) return "#00ff00";
+    return "#009c00";
+  }
 
-    return (
-        <div className="note-container">
-            <p>
-                <span className="note-score" style={{ "--bubble-color": getScoreColor(note.score) }}>{note.score}</span>
-                <span className="note-title" >&ensp;{note.title}</span>
-            </p>
-            <p className={`note-media-type ${mediaTypeClass}`}>{note.media_type}</p>
-            <p className="note-review">{note.review}</p>
-            <p className="note-date">{formattedDate}</p>
-            <button className="delete-button" onClick={() => onDelete(note.id)}>
-                Delete
-            </button>
-        </div>
-    );
+  return (
+    <div className="note-container">
+      <p>
+        <span className="note-score" style={{ "--bubble-color": getScoreColor(note.score) }}>{note.score}</span>
+        <span className="note-title" >&ensp;{note.title}</span>
+      </p>
+      <p className={`note-media-type ${mediaTypeClass}`}>{note.media_type}</p>
+      <p className="note-review">{note.review}</p>
+      <p className="note-date">{formattedDate}</p>
+      <button className="delete-button" onClick={() => onDelete(note.id)}>
+        Delete
+      </button>
+      <button className="edit-button" onClick={() => onEdit(note)}>
+        Delete
+      </button>
+    </div>
+  );
 }
 
 export default Note
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,109 +1,133 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import api from "../api";
-import Note from "../components/Note"
-import "../styles/Home.css"
+import Note from "../components/Note";
+import "../styles/Home.css";
 
 function Home() {
-    const [notes, setNotes] = useState([]);
-    const [review, setReview] = useState("");
-    const [title, setTitle] = useState("");
-    const [score, setScore] = useState("5");
-    const [media_type, setMediaType] = useState("Movie");
+  const [notes, setNotes] = useState([]);
+  const [review, setReview] = useState("");
+  const [title, setTitle] = useState("");
+  const [score, setScore] = useState("50");
+  const [media_type, setMediaType] = useState("Movie");
+  const navigate = useNavigate();
 
-    useEffect(() => {
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Note deleted!");
+        else alert("Failed to delete note.");
         getNotes();
-    }, []);
+      })
+      .catch((error) => alert(error));
+  };
 
-    const getNotes = () => {
-        api
-            .get("/api/notes/")
-            .then((res) => res.data)
-            .then((data) => {
-                setNotes(data);
-                console.log(data);
-            })
-            .catch((err) => alert(err));
-    };
+  const createNote = (e) => {
+    e.preventDefault();
 
-    const deleteNote = (id) => {
-        api
-            .delete(`/api/notes/delete/${id}/`)
-            .then((res) => {
-                if (res.status === 204) alert("Note deleted!");
-                else alert("Failed to delete note.");
-                getNotes();
-            })
-            .catch((error) => alert(error));
-    };
+    const previousNote = notes.find((note) => note.title === title && note.media_type === media_type);
+    if (!previousNote) {
+      api
+        .post("/api/notes/", { review, score, media_type, title })
+        .then((res) => {
+          if (res.status === 201) alert("Note created!");
+          else alert("Failed to make note.");
+          getNotes();
+        })
+        .catch((err) => alert(err));
+    }
+    else {
+      api
+        .put(`/api/notes/update/${previousNote.id}/`, { review, score, media_type, title })
+        .then((res) => {
+          if (res.status === 200) alert("Note updated!");
+          else alert("Failed to update note.");
+          getNotes();
+        })
+        .catch((err) => alert(err));
+    }
+  };
 
-    const createNote = (e) => {
-        e.preventDefault();
-        api
-            .post("/api/notes/", { review, score, media_type, title })
-            .then((res) => {
-                if (res.status === 201) alert("Note created!");
-                else alert("Failed to make note.");
-                getNotes();
-            })
-            .catch((err) => alert(err));
-    };
+  const editNote = (note) => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    setTitle(note.title);
+    setMediaType(note.media_type);
+    setScore(note.score.toString());
+    setReview(note.review);
+  };
 
-    return (
-        <div>
-            <button className="logout-button" type="submit" >Logout</button>
-            <h2>Create a Note</h2>
-            <form onSubmit={createNote}>
-                <label htmlFor="title">Title:</label>
-                <br />
-                <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    required
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                <label htmlFor="media-type">Media Type:</label>
-                <br />
-                <select id="media-type" name="media-type" required onChange={(e) => setMediaType(e.target.value)}>
-                    <option value="Movie">Movie</option>
-                    <option value="Book">Book</option>
-                    <option value="Album">Album</option>
-                </select>
-                <br />
-                <label htmlFor="score">Score:</label>
-                <br />
-                <input
-                    type="number"
-                    id="score"
-                    name="score"
-                    min="0"
-                    max="100"
-                    step="10"
-                    required
-                    value={score}
-                    onChange={(e) => setScore(e.target.value)}
-                />
-                <label htmlFor="review">Review:</label>
-                <br />
-                <textarea
-                    id="review"
-                    name="review"
-                    required
-                    value={review}
-                    onChange={(e) => setReview(e.target.value)}
-                ></textarea>
-                <br />
-                <input type="submit" value="Submit"></input>
-            </form>
-            <div>
-                <h2>Notes</h2>
-                {notes.map((note) => (
-                    <Note note={note} onDelete={deleteNote} key={note.id} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <button className="logout-button" onClick={() => navigate("/logout")} >Logout</button>
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="media-type">Media Type:</label>
+        <br />
+        <select id="media-type" name="media-type" required onChange={(e) => setMediaType(e.target.value)}>
+          <option value="Movie">Movie</option>
+          <option value="Book">Book</option>
+          <option value="Album">Album</option>
+        </select>
+        <br />
+        <label htmlFor="score">Score:</label>
+        <br />
+        <input
+          type="number"
+          id="score"
+          name="score"
+          min="0"
+          max="100"
+          step="10"
+          required
+          value={score}
+          onChange={(e) => setScore(e.target.value)}
+        />
+        <label htmlFor="review">Review:</label>
+        <br />
+        <textarea
+          id="review"
+          name="review"
+          required
+          value={review}
+          onChange={(e) => setReview(e.target.value)}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit"></input>
+      </form>
+      <div>
+        <h2>Notes</h2>
+        {notes.map((note) => (
+          <Note note={note} onDelete={deleteNote} onEdit={editNote} key={note.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
